19bdb7ef9123980512e2e988ccc33de2c35bf1dd
martineliasq <elias.quintero@udea.edu.co>
2019-05-10 15:36:26 -0500

    add dims

--- cactus/face/strategy/dnn.py
+++ cactus/face/strategy/dnn.py
@@ -10,6 +10,7 @@ SSD_NET = cv2.dnn.readNetFromCaffe(PROTOTXT, CAFFE_MODEL)
 
 def dnn(imagen):
     img = read(imagen)
+    img_width, img_height, _ = img.shape
     img_blob = to_blob(img)
     SSD_NET.setInput(img_blob)
     detections = SSD_NET.forward()
@@ -18,6 +19,7 @@ def dnn(imagen):
     for i in range(0, detections.shape[2]):
         confidense = detections[0, 0, i, 2]
         if confidense > 0.7:
-            box = detections[0, 0, i, 3:7]*np.array([w, h, w, h])
+            box = detections[0, 0, i, 3:7] * \
+                np.array([img_width, img_height, img_width, img_height])
             boxes.append(box.astype('int'))
     return boxes
