508fcfcce94c23a1df22497a5919065d70813fb3
martineliasq <elias.quintero@udea.edu.co>
2019-05-10 15:22:36 -0500

    Haar Funtion

---
+++ cactus/face/strategy/haar.py
@@ -0,0 +1,24 @@
+from cactus.utils.image import (read, to_gray)
+import cv2
+import numpy as np
+
+CASCADE_XML = "src/"
+FACE_DETECTOR = cv2.CascadeClassifier(CASCADE_XML)
+
+
+def haar(imagen):
+    img = read(imagen)
+    img_width, img_height = img.shape
+    img_gray = to_gray(img)
+    detected_faces = FACE_DETECTOR.detectMultiScale(img_gray, 1.3, 5)
+    boxes = list()
+
+    for (x, y, w, h) in detected_faces:
+        center_x = x + (w/2)
+        center_y = y + (h/2)
+        b_dim = min(max(w, h)*1.3, img_width, img_height)
+        box = ((center_x-b_dim/2), (center_y - b_dim/2),
+               (center_x+b_dim/2), center_y + b_dim/2)
+
+        boxes.append(np.array(box))
+    return boxes
