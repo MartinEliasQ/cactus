55d4f4e013135635d4384ff3794170891377d492
martineliasq <elias.quintero@udea.edu.co>
2019-05-10 15:08:49 -0500

    hog funtion

--- cactus/face/strategy/hog.py
+++ cactus/face/strategy/hog.py
@@ -1,5 +1,22 @@
+import dlib
+from cactus.utils.image import (read, to_gray)
+import numpy as np
+
+FACE_DETECTOR = dlib.get_frontal_face_detector()
+
 
 def hog(imagen):
+    img = read(imagen)
+    img_gray = to_gray(img)
+    detected_faces = FACE_DETECTOR(img_gray, 1)
     boxes = list()
 
-    pass
+    for _, face_box in enumerate(detected_faces):
+        x = face_box.left()
+        y = face_box.top()
+        w = face_box.right()
+        h = face_box.bottom()
+
+        box = (x, y, w, h)
+        boxes.append(np.array(box))
+    return boxes
